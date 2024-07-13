fbfd07ea51fe9e256ee3f7f9e505f396ee6df1ac
carloscardoso-prog <carloseduardocardsilva@gmail.com>
2024-07-13 16:37:55 -0300

    binary search recursive

--- binary_search_recursive.php
+++ binary_search_recursive.php
@@ -1,21 +1,28 @@
 <?php
 
 $array = [1,2,3,4,5,6,7,8,9];
+$target = 7;
 
-function searchBinary($array, $target){
-    if($array[0] == $target){
-        return $array[0];
-    }
-    $arraySize = round(count($array)/2);
-    if($arraySize > $target){
-        $array = array_slice($array, $arraySize);
+$smaller = 0;
+$bigger = count($array)-1;
+
+function searchBinary($array, $smaller, $bigger, $target){
+    $guess = floor(($bigger + $smaller)/2);
+
+    if($target == $array[$guess]){
+        print_r($array[$guess]);
+        die;
     }
 
-    if($arraySize < $target){
-        $array = array_slice($array, 0, $arraySize);
+    if($target > $array[$guess]){
+        $smaller = $guess+1;    
     }
 
-    searchBinary($array, $target);
+    if($target < $array[$guess]){
+        $bigger = $guess-1;
+    }
+
+    searchBinary($array, $smaller, $bigger, $target);
 }
 
-print_r(searchBinary($array, 3));
+print_r(searchBinary($array, $smaller, $bigger, $target));
